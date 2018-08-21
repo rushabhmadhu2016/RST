270231f52e7c03fe61e783481bde555b3dcae929
Rushabh Madhu <rushabh.madhu@innvonix.com>
2018-08-21 18:18:54 +0530

    claimed page issue fix

--- myapp/app/controllers/claimController.js
+++ myapp/app/controllers/claimController.js
@@ -66,65 +66,53 @@ exports.ignoreClaim = function(req, res) {
 	});
 }
 
-exports.getClaimedLocations = function(req,res){
+exports.getClaimedLocations = async function(req,res){
 	var userIds = [];
 	var propertyIds = [];
 	var usersList = [];
 	var category_filter = {};
 	var filters = {};
 	if(req.query.category_filter) {	filters.category_filter=req.query.category_filter;	}
-	Claim.find({status:0}, function(err, claimedproperties) {
-	   	claimedproperties.forEach(function(cproperty) {
-	   		userIds.push(cproperty.user_id);
-	   		propertyIds.push(cproperty.property_id);	  		
-		});		
-		propertyList = [];		
-		Property.find({id:{ $in: propertyIds }}, function(err, properties) {				
-			properties.forEach(function(property) {
-				propertyList[property.id]=property;
-				userIds.push(property.user_id);
-			});			
-			usersList = [];
-			User.find({id:{ $in: userIds}}, function(err, users){
-				users.forEach(function(user) {	      	
-			      	usersList[user.id] = [];
-			      	usersList[user.id]["first_name"] = user.first_name;
-			      	usersList[user.id]["last_name"] = user.last_name;
-			      	usersList[user.id]["mail"] = user.mail;
-			      	usersList[user.id]["contact_number"] = user.contact_number;
-				});
-				console.log(usersList);
-				claimedLocationsList = [];
-				claimedproperties.forEach(function(cproperty) {
-					claimedLocation = {};
-			   		claimedLocation.id=cproperty.id;
-			   		claimedLocation.property_id=cproperty.property_id;
-			   		claimedLocation.property_name=propertyList[cproperty.property_id].property_name;
-			   		claimedLocation.category=propertyList[cproperty.property_id].category_id;
-			   		claimedLocation.contact_number=usersList[cproperty.user_id].contact_number;
-			   		claimedLocation.exiting_owner_id=propertyList[cproperty.property_id].user_id;
-			   		claimedLocation.exiting_owner=usersList[propertyList[cproperty.property_id].user_id].first_name;
-			   		claimedLocation.mail=usersList[propertyList[cproperty.property_id].user_id].mail;
-			   		claimedLocation.new_owner_id=cproperty.user_id;
-			   		claimedLocation.new_owner_name=usersList[cproperty.user_id].first_name;
-			   		claimedLocationsList.push(claimedLocation);
-				});
-				Category.find({}, function(err, category){
-					categories = [];
-					category.forEach(function(category){
-						categories[category.id]=category;
-					});
-					res.render('admin/allClaimedProperties.ejs', {
-						error : req.flash("error"),
-						success: req.flash("success"),					
-						property: claimedLocationsList,
-						categories: categories,
-						filters: filters,
-					});
-				})
-			});
-		});
+
+	let claimedpropertiesData = await Claim.find({status:0}).populate({path: 'user',
+      model: 'User',select: 'first_name last_name mail id contact_number'}).populate({path: 'property', model: 'Property',select:'id property_name category_id user'});
+	let existinguser = '';
+	claimedLocationsList = [];
+	let user_ids = [];
+	claimedpropertiesData.forEach(function(cproperty) {
+		user_ids.push(cproperty.property.user);
 	});
+	let usersData = await User.find({_id:{$in: user_ids}}).select('first_name last_name mail id');
+	var existingusers = [];
+	usersData.forEach(function(usr){
+		existingusers[usr._id]=usr;
+	});
+	claimedpropertiesData.forEach(function(cproperty) {
+		claimedLocation = {};
+   		claimedLocation.id=cproperty.id;
+   		claimedLocation.property_id=cproperty.property_id;
+   		claimedLocation.property_name=cproperty.property.property_name;
+   		claimedLocation.category=cproperty.property.category_id;
+   		claimedLocation.contact_number=cproperty.user.contact_number;
+   		claimedLocation.exiting_owner_id=cproperty.property.user_id;
+   		claimedLocation.exiting_owner=existingusers[cproperty.property.user._id].first_name;
+   		claimedLocation.mail=cproperty.user.mail;
+   		claimedLocation.new_owner_id=cproperty.user_id;
+   		claimedLocation.new_owner_name=cproperty.user.first_name+' '+cproperty.user.last_name;
+   		claimedLocationsList.push(claimedLocation);
+	});
+	let category = await Category.find({});
+	categories = [];
+	category.forEach(function(category){
+		categories[category.id]=category;
+	});
+	res.render('admin/allClaimedProperties.ejs', {
+		error : req.flash("error"),
+		success: req.flash("success"),					
+		property: claimedLocationsList,
+		categories: categories,
+		filters: filters,
+	});	
 }
 
 function getDate(){ var d = new Date(); return d.getFullYear()+ '-'+addZero(d.getMonth())+'-'+addZero(d.getDate())+' '+addZero(d.getHours())+':'+addZero(d.getMinutes())+':'+addZero(d.getSeconds()); } 
